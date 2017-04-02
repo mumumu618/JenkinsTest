eedc7c31ebea737cd7297188ecf37a512befc0ab
Shibainu <shibainutool@gmail.com>
2017-04-03 02:43:46 +0900

    no message

---
+++ Assets/Editor/BatchBuild/BatchBuild.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BatchBuild : MonoBehaviour
+{
+
+    //for Android
+    static void BuildProject_Android()
+    {
+
+        Debug.Log("AndroidBuild...");
+
+
+        string[] scenePaths = GetEnabledScenePaths();
+        //string outputPath = Application.dataPath + "/../TestAPK.apk";
+        string outputPath = "C:/Users/Shibainu/Documents/Unity/JenkinsTest/TestAPK.apk";
+        BuildTarget target = BuildTarget.Android;
+        BuildOptions opt = BuildOptions.None;
+
+        ApplicationBuild(scenePaths, outputPath, target, opt);
+    }
+
+    //for iOS Device
+    //static void BuildProject_iOS()
+    //{
+
+    //    Debug.Log("iOSBuild...");
+
+    //    string[] scenePaths = GetEnabledScenePaths();
+    //    string outputPath = "Device";
+    //    BuildTarget target = BuildTarget.iOS;
+    //    BuildOptions opt = BuildOptions.SymlinkLibraries;
+
+    //    //ProjectDataSize Reduction
+    //    EditorUserBuildSettings.symlinkLibraries = true;
+    //    //Prevent SimulatorBuild
+    //    PlayerSettings.iOS.sdkVersion = iOSSdkVersion.DeviceSDK;
+
+    //    ApplicationBuild(scenePaths, outputPath, target, opt);
+    //}
+
+    //RunBuild
+    static void ApplicationBuild(string[] scenePaths, string outputPath, BuildTarget target, BuildOptions opt)
+    {
+
+        string error = BuildPipeline.BuildPlayer(scenePaths, outputPath, target, opt);
+
+        if (!string.IsNullOrEmpty(error))
+            Debug.LogError(error);      //BuildFailed
+        EditorApplication.Exit(string.IsNullOrEmpty(error) ? 0 : 1);
+
+    }
+
+    //return Enabled Scene in BuildSettings's Scenes In Build 
+    static string[] GetEnabledScenePaths()
+    {
+
+        List<string> sceneList = new List<string>();
+
+        //Check Scenes Enabled
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+                sceneList.Add(scene.path);
+        }
+        return sceneList.ToArray();
+    }
+
+}
